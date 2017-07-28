e41989d6da649890fd7b78d00034ba291a4cb9be
Simon Weller <s.meierbeck@gmail.com>
2017-07-28 17:34:07 +0200

    Chapter 1: Scopes and Dirty-Checking completed

--- src/scope.js
+++ src/scope.js
@@ -5,38 +5,73 @@ var initWatchVal = function () { };
 
 function Scope() {
   this.$$watchers = [];
+  this.$$lastDirtyWatch = null;
 }
 
-Scope.prototype.$watch = function(watchFn, listenerFn) {
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
+  var self = this;
   var watcher = {
     watchFn: watchFn,
     listenerFn: listenerFn || function() { },
+    valueEq: Boolean(valueEq),
     last: initWatchVal
   };
-  this.$$watchers.push(watcher);
+  this.$$watchers.unshift(watcher);
+  this.$$lastDirtyWatch = null; // resetting to ensure new watchers always run
+  return function() { // returning function to remove this watcher
+      var index = self.$$watchers.indexOf(watcher);
+      if (index >= 0) {
+        self.$$watchers.splice(index, 1);
+        self.$$lastDirtyWatch = null; // reset so a clean watcher that has deleted the next one
+        // doesn't short circuit the digest when it is called a second time;
+      }
+  };
+};
+
+Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
+  if (valueEq) {
+    return _.isEqual(newValue, oldValue);
+  } else {
+    return newValue === oldValue || (typeof oldValue ==="number" &&
+      typeof newValue === "number" && isNaN(oldValue) && isNaN(newValue));
+  }
 };
 
 Scope.prototype.$$digestOnce = function() {
   var self = this;
   var newValue, oldValue, dirty;
-  _.forEach(this.$$watchers, function(watcher) {
-    newValue = watcher.watchFn(self);
-    oldValue = watcher.last;
-    if (newValue !== oldValue) {
-      watcher.last = newValue;
-      watcher.listenerFn(newValue,
-        (oldValue === initWatchVal ? newValue : oldValue)
-        , self);
-      dirty = true;
+  _.forEachRight(this.$$watchers, function(watcher) {
+    try {
+      if (watcher) { // make sure it hasn't been deleted by another watcher
+        newValue = watcher.watchFn(self);
+        oldValue = watcher.last;
+        if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+          self.$$lastDirtyWatch = watcher;
+          watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
+          watcher.listenerFn(newValue,
+            (oldValue === initWatchVal ? newValue : oldValue),
+            self);
+          dirty = true;
+        } else if (self.$$lastDirtyWatch === watcher) { // short circuit if last dirty watch is clean
+          return false;
+        }
+      }
+    } catch (e) {
+      console.error(e);
     }
   });
   return dirty;
 };
 
 Scope.prototype.$digest = function() {
+  var ttl = 10;
   var dirty;
+  this.$$lastDirtyWatch = null;
   do {
     dirty = this.$$digestOnce();
+    if (dirty && !(ttl--)) {
+      throw '10 digest iterations reached';
+    }
   } while(dirty);
 };
 
