fd2dca3cc02828f76f62be1014ecfbd62393d77e
Simon Weller <s.meierbeck@gmail.com>
2017-07-28 14:31:39 +0200

    Scopes and Dirty-Checking: Unstable Digest

---
+++ src/scope.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var _ = require('lodash');
+var initWatchVal = function () { };
+
+function Scope() {
+  this.$$watchers = [];
+}
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn || function() { },
+    last: initWatchVal
+  };
+  this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$$digestOnce = function() {
+  var self = this;
+  var newValue, oldValue, dirty;
+  _.forEach(this.$$watchers, function(watcher) {
+    newValue = watcher.watchFn(self);
+    oldValue = watcher.last;
+    if (newValue !== oldValue) {
+      watcher.last = newValue;
+      watcher.listenerFn(newValue,
+        (oldValue === initWatchVal ? newValue : oldValue)
+        , self);
+      dirty = true;
+    }
+  });
+  return dirty;
+};
+
+Scope.prototype.$digest = function() {
+  var dirty;
+  do {
+    dirty = this.$$digestOnce();
+  } while(dirty);
+};
+
+module.exports = Scope;
